0b2cbe0d237e7d8504a21799153c2cfcfe1fa378
miriamRichter94 <dark.rose2094@gmail.com>
2026-04-07 15:31:07 +0200

    Setup base database connection

---
+++ pages/api/plants/index.js
@@ -0,0 +1,17 @@
+import dbConnect from "@/db/dbConnect";
+import Plant from "@/db/models/Plant";
+
+export default async function handler(request, response) {
+  await dbConnect();
+
+  console.log(request.method);
+
+  if (request.method === "GET") {
+    const plants = await Plant.find();
+
+    response.status(200).json(plants);
+    return;
+  }
+
+  response.status(405).json({ status: "Method not allowed." });
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+import useSWR from "swr";
+
 export default function HomePage() {
+  const { data: plants, isLoading } = useSWR("/api/plants");
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!plants) {
+    return;
+  }
+
   return (
     <div>
       <h1>Hello from Next.js</h1>
+      <ul>
+        {plants.map((plant) => (
+          <li key={plant._id}>{plant.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
