d781f8a00293902180a7554f3d15e438050ae3be
miriamRichter94 <dark.rose2094@gmail.com>
2026-04-13 16:47:29 +0200

    Adding Links for Bookmarks

--- pages/bookmarks.js
+++ pages/bookmarks.js
@@ -1,4 +1,5 @@
 import PlantList from "@/Components/PlantList/PlantList";
+import Link from "next/link";
 
 export default function Bookmarks({
   plants,
@@ -11,6 +12,7 @@ export default function Bookmarks({
   return (
     <>
       <h1>Bookmarked Plants</h1>
+      <Link href="/">Back To Plant List</Link>
       {bookmarkedPlants.length === 0 ? (
         <p>No Bookmarked Plants found</p>
       ) : (
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Filter from "@/Components/Filter/Filter";
 import PlantForm from "@/Components/PlantForm/PlantModal";
 import PlantList from "@/Components/PlantList/PlantList";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function HomePage({
@@ -18,6 +19,8 @@ export default function HomePage({
     <>
       <h1>Plant Pal</h1>
       <PlantForm />
+
+      <Link href="/bookmarks">To bookmarked plants</Link>
       <Filter
         selectedFilter={selectedFilter}
         setSelectedFilter={setSelectedFilter}
